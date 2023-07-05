9250008476edcd4cb7a8b678cf70ed642d03a2c3
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-05 09:07:19 +0545

    Store and iterate through sequences of data using Arrays and the foreach statement

---
+++ Learn/Microsoft/Store and iterate through sequences of data using Arrays and the foreach statement/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] fraudulentOrderIds = {"B123",
+"C234",
+"A345",
+"C15",
+"B177",
+"G3003",
+"C235",
+"B179"};
+
+foreach (string fraudulentOrderId in fraudulentOrderIds)
+{
+    if (fraudulentOrderId.StartsWith("B"))
+    {
+        Console.WriteLine(fraudulentOrderId);
+    }
+}
