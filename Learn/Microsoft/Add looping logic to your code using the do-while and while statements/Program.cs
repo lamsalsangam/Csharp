649a1ce9fc46d0b0c41149bd6742c489bb3c77bf
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-17 06:41:26 +0545

    13. code that validates integer input

--- Learn/Microsoft/Add looping logic to your code using the do-while and while statements/Program.cs
+++ Learn/Microsoft/Add looping logic to your code using the do-while and while statements/Program.cs
@@ -1,20 +1,58 @@
-﻿int hero = 10;
-int monster = 10;
+﻿// int hero = 10;
+// int monster = 10;
 
-Random dice = new Random();
+// Random dice = new Random();
+
+// do
+// {
+//     int roll = dice.Next(1, 11);
+//     monster -= roll;
+//     Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+//     if (monster <= 0) continue;
+
+//     roll = dice.Next(1, 11);
+//     hero -= roll;
+//     Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+// } while (hero > 0 && monster > 0);
+
+// Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+
+
+// ########################################################################################################
+
+string? readResult;
+string valueEntered = "";
+int numValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
 
 do
 {
-    int roll = dice.Next(1, 11);
-    monster -= roll;
-    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        valueEntered = readResult;
+    }
 
-    if (monster <= 0) continue;
+    validNumber = int.TryParse(valueEntered, out numValue);
 
-    roll = dice.Next(1, 11);
-    hero -= roll;
-    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+    if (validNumber == true)
+    {
+        if (numValue <= 5 || numValue >= 10)
+        {
+            validNumber = false;
+            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+} while (validNumber == false);
 
-} while (hero > 0 && monster > 0);
+Console.WriteLine($"Your input value ({numValue}) has been accepted.");
 
-Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+readResult = Console.ReadLine();
