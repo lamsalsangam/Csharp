eb9a4c386dd8ea2f1a3b03e4f6767fe6da9a51ac
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-18 19:09:56 +0545

    14. testing some of the methods

--- Learn/Microsoft/Add looping logic to your code using the do-while and while statements/Program.cs
+++ Learn/Microsoft/Add looping logic to your code using the do-while and while statements/Program.cs
@@ -22,37 +22,76 @@
 
 // ########################################################################################################
 
-string? readResult;
-string valueEntered = "";
-int numValue = 0;
-bool validNumber = false;
+// string? readResult;
+// string valueEntered = "";
+// int numValue = 0;
+// bool validNumber = false;
 
-Console.WriteLine("Enter an integer value between 5 and 10");
+// Console.WriteLine("Enter an integer value between 5 and 10");
 
-do
+// do
+// {
+//     readResult = Console.ReadLine();
+//     if (readResult != null)
+//     {
+//         valueEntered = readResult;
+//     }
+
+//     validNumber = int.TryParse(valueEntered, out numValue);
+
+//     if (validNumber == true)
+//     {
+//         if (numValue <= 5 || numValue >= 10)
+//         {
+//             validNumber = false;
+//             Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//     }
+// } while (validNumber == false);
+
+// Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+// readResult = Console.ReadLine();
+
+// ########################################################################################################
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
 {
-    readResult = Console.ReadLine();
-    if (readResult != null)
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+
+    // extract sentences from each string and display them one at a time
+    while (periodLocation != -1)
     {
-        valueEntered = readResult;
+
+        // first sentence is the string value to the left of the period location
+        mySentence = myString.Remove(periodLocation);
+
+        // the remainder of myString is the string value to the right of the location
+        myString = myString.Substring(periodLocation + 1);
+
+        // remove any leading white-space from myString
+        myString = myString.TrimStart();
+
+        // update the comma location and increment the counter
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
     }
 
-    validNumber = int.TryParse(valueEntered, out numValue);
-
-    if (validNumber == true)
-    {
-        if (numValue <= 5 || numValue >= 10)
-        {
-            validNumber = false;
-            Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again");
-    }
-} while (validNumber == false);
-
-Console.WriteLine($"Your input value ({numValue}) has been accepted.");
-
-readResult = Console.ReadLine();
+    // the remaining portion of speciesToListSelection is the final species name 
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
