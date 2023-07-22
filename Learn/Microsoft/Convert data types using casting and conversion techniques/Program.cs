58d15cc796b672309f59f95633f6dc43edf6579a
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-22 17:34:45 +0545

    17. Convert data types using casting and conversion techniques 2

--- Learn/Microsoft/Convert data types using casting and conversion techniques/Program.cs
+++ Learn/Microsoft/Convert data types using casting and conversion techniques/Program.cs
@@ -1,20 +1,38 @@
-﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+﻿// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-decimal total = 0m;
-string message = "";
+// decimal total = 0m;
+// string message = "";
 
-foreach (var value in values)
-{
-    decimal number; // stores the TryParse "out" value
-    if (decimal.TryParse(value, out number))
-    {
-        total += number;
-    }
-    else
-    {
-        message += value;
-    }
-}
+// foreach (var value in values)
+// {
+//     decimal number; // stores the TryParse "out" value
+//     if (decimal.TryParse(value, out number))
+//     {
+//         total += number;
+//     }
+//     else
+//     {
+//         message += value;
+//     }
+// }
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
+
+
+// ############################################################################
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+
+// Convert.ToInt32() rounds up.
+int result1 = Convert.ToInt32((decimal)value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
