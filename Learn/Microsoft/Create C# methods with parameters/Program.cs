ea94975597705fe804253a81a91bc74642c14b30
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-30 18:28:29 +0545

    24. Create C# methods with parameters 2

--- Learn/Microsoft/Create C# methods with parameters/Program.cs
+++ Learn/Microsoft/Create C# methods with parameters/Program.cs
@@ -27,57 +27,90 @@
 
 // ######################################################################
 
-string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
-string[] rsvps = new string[10];
-int count = 0;
+// string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+// string[] rsvps = new string[10];
+// int count = 0;
 
-void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// {
+//     if (inviteOnly)
+//     {
+//         bool found = false;
+//         foreach (string guest in guestList)
+//         {
+//             if (guest.Equals(name))
+//             {
+//                 found = true;
+//                 break;
+//             }
+//         }
+//         if (!found)
+//         {
+//             Console.WriteLine($"Sorry, {name} is not on the guest list");
+//             return;
+//         }
+//     }
+
+//     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+//     count++;
+// }
+
+// void ShowRSVPs()
+// {
+//     Console.WriteLine("\nTotal RSVPs:");
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.WriteLine(rsvps[i]);
+//     }
+// }
+
+// RSVP("Rebecca", 1, "none", true);
+// RSVP("Nadia", 2, "Nuts", true);
+// RSVP("Linh", 2, "none", false);
+// RSVP("Tony", 1, "Jackfruit", true);
+// RSVP("Noor", 4, "none", false);
+// RSVP("Jonte", 2, "Stone fruit", false);
+// ShowRSVPs();
+
+// RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
+// ShowRSVPs();
+
+// RSVP("Rebecca");
+// RSVP("Nadia", 2, "Nuts");
+// RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+// RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+// RSVP("Noor", 4, inviteOnly: false);
+// RSVP("Jonte", 2, "Stone fruit", false);
+
+// ######################################################################
+
+string[,] corporate =
 {
-    if (inviteOnly)
-    {
-        bool found = false;
-        foreach (string guest in guestList)
-        {
-            if (guest.Equals(name))
-            {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-        {
-            Console.WriteLine($"Sorry, {name} is not on the guest list");
-            return;
-        }
-    }
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
 
-    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-    count++;
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayEmail(first: corporate[i, 0], last: corporate[i, 1]);
 }
 
-void ShowRSVPs()
+for (int i = 0; i < external.GetLength(0); i++)
 {
-    Console.WriteLine("\nTotal RSVPs:");
-    for (int i = 0; i < count; i++)
-    {
-        Console.WriteLine(rsvps[i]);
-    }
+    DisplayEmail(first: external[i, 0], last: external[i, 1], domain: externalDomain);
 }
 
-RSVP("Rebecca", 1, "none", true);
-RSVP("Nadia", 2, "Nuts", true);
-RSVP("Linh", 2, "none", false);
-RSVP("Tony", 1, "Jackfruit", true);
-RSVP("Noor", 4, "none", false);
-RSVP("Jonte", 2, "Stone fruit", false);
-ShowRSVPs();
-
-RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
-ShowRSVPs();
-
-RSVP("Rebecca");
-RSVP("Nadia", 2, "Nuts");
-RSVP(name: "Linh", partySize: 2, inviteOnly: false);
-RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
-RSVP("Noor", 4, inviteOnly: false);
-RSVP("Jonte", 2, "Stone fruit", false);
+void DisplayEmail(string first, string last, string domain = "contoso.com")
+{
+    string email = first.Substring(0, 2) + last;
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{domain}");
+}
