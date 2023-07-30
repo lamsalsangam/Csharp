593e85bd977553e6c0322e8c42b9bad90f2196d6
lamsalsangam <me.sangam.lamsal.10@gmail.com>
2023-07-30 18:09:29 +0545

    24. Create C# methods with parameters 1

---
+++ Learn/Microsoft/Create C# methods with parameters/Program.cs
@@ -0,0 +1,83 @@
+﻿// int[] schedule = { 800, 1200, 1600, 2000 };
+
+// void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
+// {
+//     int diff = 0;
+//     if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+//     {
+//         Console.WriteLine("Invalid GMT");
+//     }
+//     else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//     {
+//         diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+//     }
+//     else
+//     {
+//         diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//     }
+
+//     for (int i = 0; i < times.Length; i++)
+//     {
+//         int newTime = ((times[i] + diff)) % 2400;
+//         Console.WriteLine($"{times[i]} -> {newTime}");
+//     }
+// }
+
+// DisplayAdjustedTimes(schedule, 6, -6);
+
+// ######################################################################
+
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+{
+    if (inviteOnly)
+    {
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
+}
+
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP("Linh", 2, "none", false);
+RSVP("Tony", 1, "Jackfruit", true);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
+ShowRSVPs();
+
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
